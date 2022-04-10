1c197a47e08181a4be731b295989be42965e1d11
Neil Lawrence <lawrennd@gmail.com>
2022-04-10 12:10:11 +0100

    General update

--- sscholar/semantic.py
+++ sscholar/semantic.py
@@ -17,6 +17,29 @@ class Client():
         return self._collection.find(query)
 
 
+    def _find_files():
+        """Find all files in the data downloaded from semantic scholar."""
+        from pathlib import Path
 
+        self._data_files = []
+        for path in Path(config.semantic_dir).rglob('*.gz'):
+            data_files.append(path.name)
+        self._data_files.sort()
+    
+
+for file in files[3758:]:
+    if file in ['license.txt', 'manifest.txt', 's2-corpus-1001']:
+        continue
+    with gzip.open(os.path.join(semantic_dir, file)) as f:
+        it = jsonstream.load(f)
+        while True:
+            try:
+                collection.insert_one(next(it))
+            except StopIteration as e:
+                print('completed file ' + file)
+                break
+            except json.JSONDecodeError:
+                missing_files.append(file)
+                print('Decode Error file:' + file)
     
     
